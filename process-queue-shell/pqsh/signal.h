4bbbbc4dba05214ff4ffa02da5af6a01951f5708
Alex <78465604+alex-clinton@users.noreply.github.com>
2023-01-13 16:01:22 -0500

    Signal

---
+++ process-queue-shell/pqsh/signal.h
@@ -0,0 +1,21 @@
+/* signal.h: PQSH Signal Handlers */
+
+#ifndef PQSH_SIGNAL_H
+#define PQSH_SIGNAL_H
+
+#include <signal.h>
+#include <stdbool.h>
+
+#ifndef GNU_SOURCE
+typedef void (*sighandler_t)(int);
+#endif
+
+/* Functions */
+
+bool    signal_register(int signum, int flags, sighandler_t handler);
+
+/* Handlers */
+
+void	sigalrm_handler(int signum);
+
+#endif
